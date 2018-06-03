aaa2bf9e332ef878ff4fb9e150fc8bb54916f038
T L <tom.kennedy.lee@gmail.com>
2018-06-03 01:10:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ function logWhisper() {
 }
 
 function sayHiToGrandma(string) {
-  if (string == whisper(string))
+  if (string == whisper(string)) 
     return "I can't hear you!"
-}
+  else if (string == shout(string))     
+  return "YES INDEED!"
+  else (string = "I love you, Grandma.")
+  return "I love you too."
+  }
+    
     
